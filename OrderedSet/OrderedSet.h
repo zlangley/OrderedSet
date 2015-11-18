2f22fc4e0397e81c96db7a0eb3b2e4dd17a72053
zlangley <zach@operator.com>
2015-11-18 10:40:12 -0800

    Initial Commit

---
+++ OrderedSet/OrderedSet.h
@@ -0,0 +1,19 @@
+//
+//  OrderedSet.h
+//  OrderedSet
+//
+//  Created by Zach Langley on 11/18/15.
+//  Copyright © 2015 Operator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrderedSet.
+FOUNDATION_EXPORT double OrderedSetVersionNumber;
+
+//! Project version string for OrderedSet.
+FOUNDATION_EXPORT const unsigned char OrderedSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrderedSet/PublicHeader.h>
+
+
